6fd9090fa58d4f98485a959728c7d63ef4b36807
Alexander Densley <alexander.densley@byu.edu>
2022-08-16 16:48:03 -0600

    play again and random number features added

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 let min = 1,
     max = 10,
-    winningNum = 2,
+    winningNum = Math.floor(Math.random() * (max - min +1) ) + min,
     guessesLeft = 3;
+
+console.log(winningNum)
 
 const game = document.getElementById('game'),
       minNum = document.querySelector('.min-num'),
@@ -13,6 +15,12 @@ const game = document.getElementById('game'),
 minNum.textContent = min;
 maxNum.textContent = max;
 
+game.addEventListener('mousedown',function(e){
+    if(e.target.className === 'play-again'){
+        window.location.reload()
+    }
+})
+
 guessBtn.addEventListener('click', function(){
     let guess = parseInt(guessInput.value);
     if(guess > max || guess < min || isNaN(guess)){
@@ -21,22 +29,29 @@ guessBtn.addEventListener('click', function(){
     else if (guess === winningNum){
         guessInput.disabled = true
         guessInput.style.borderColor = 'green';
-        setMessage(`${guess} is correct!`, 'green')
+        setMessage(`${guess} is correct, You win!`, 'green')
+        gameOver()
     }
     else{
         guessesLeft--;
         guessInput.style.borderColor = 'red'
         if(guessesLeft === 0){
             guessInput.disabled = true
-            setMessage(`You lost, the correct number was ${winningNum}.`)
+            setMessage(`You lost, the correct number was ${winningNum}.`, 'red')
+            gameOver()
         }
         else{
             guessInput.value = ''
-            setMessage(`${guess} is incorrect. You have ${guessesLeft} left.` )
+            setMessage(`${guess} is incorrect. You have ${guessesLeft} left.`,'red')
         }
         
     }
 })
+
+function gameOver(){ 
+    guessBtn.value = "Play Again"
+    guessBtn.className += 'play-again'
+}
 
 function setMessage(msg, color){
     message.style.color = color
